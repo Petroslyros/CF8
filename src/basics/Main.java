5e55c9d2a0196fe5edeeb11c0ad6ca8162a423f4
Petros <petrosluros@gmail.com>
2025-05-10 15:07:08 +0300

    edits in main

--- src/basics/Main.java
+++ src/basics/Main.java
@@ -1,5 +1,6 @@
 package basics;
 
+import java.util.Arrays;
 import java.util.Scanner;
 
 public class Main {
@@ -50,6 +51,7 @@ public class Main {
 //                System.out.print(s.substring(i, i+1));
 //            }
 //        }
+
 //        int positionOfLasto = cf.lastIndexOf("o"); //11
 
 
@@ -193,11 +195,42 @@ public class Main {
             char letter = s.charAt(i);
             if(letter == 'z'){
                 sb.append('a');
-            } else sb.append((char) (letter + 1));
+            } else sb.append((char) (letter + 3));
         }
         return sb.toString();
     }
     public static boolean isInteger(String s) {
         return s.matches("-?\\d+");
+    }
+    public static int maxCarsAtSameTime(int[][] arr) {
+        int n = arr.length;
+        int[][] events = new int[n * 2][2];
+
+        // Step 1: Fill events array with arrival (1) and departure (0)
+        for (int i = 0; i < n; i++) {
+            events[i * 2][0] = arr[i][0];
+            events[i * 2][1] = 1;  // arrival
+            events[i * 2 + 1][0] = arr[i][1];
+            events[i * 2 + 1][1] = 0;  // departure
+        }
+
+        // Step 2: Sort events by time; if equal, departures first
+        Arrays.sort(events, (a, b) -> {
+            if (a[0] != b[0]) return a[0] - b[0];
+            return a[1] - b[1]; // departure before arrival if time is the same
+        });
+
+        // Step 3: Traverse events and compute max count
+        int count = 0, maxCount = 0;
+        for (int[] event : events) {
+            if (event[1] == 1) {
+                count++; // arrival
+                maxCount = Math.max(maxCount, count);
+            } else {
+                count--; // departure
+            }
+        }
+
+        return maxCount;
     }
 }
--- src/basics/PracticeBlank.java
+++ src/basics/PracticeBlank.java
@@ -4,8 +4,24 @@ public class PracticeBlank {
     public static void main(String[] args) {
 
 
+
+        }
+    public static String caesarEncrypt(String s){
+        StringBuilder sb = new StringBuilder();
+
+        for(int i = 0; i < s.length(); i++){
+            char letter = s.charAt(i);
+            if(letter == 'z'){
+                sb.append('a');
+            } else {
+                sb.append((char) (letter + 3));
+            }
+        }
+        return sb.toString();
+    }
+
     }
 
 
 
-}
+
