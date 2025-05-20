52830adeceb271ba639ac5b1fb091d476551b34e
Petros <petrosluros@gmail.com>
2025-05-20 20:21:22 +0300

    user class

--- src/gr/aueb/cf/ch12/Main.java
+++ src/gr/aueb/cf/ch12/Main.java
@@ -4,16 +4,21 @@ public class Main {
     public static void main(String[] args) {
 
         Student alice = new Student();
+        Student bob = new Student(2, "Bob", "D.");
+        bob.setId(23);
+
 
         //GET
-        alice.id = 10;
-        alice.firstname = "Alice";
-        alice.lastname = "W.";
+        alice.setId(10);
+        alice.setFirstname("Alice");
+        alice.setLastname("W.");
 
         //SET
-        System.out.println("ID: " + alice.id);
-        System.out.println("Firstname: " + alice.firstname);
-        System.out.println("Lastname: " + alice.lastname);
+        System.out.println("ID: " + alice.getId());
+        System.out.println("Firstname: " + alice.getFirstname());
+        System.out.println("Lastname: " + alice.getLastname());
+
+        bob.setLastname("Dylan");
 
     }
 }
--- src/gr/aueb/cf/ch12/Student.java
+++ src/gr/aueb/cf/ch12/Student.java
@@ -1,7 +1,42 @@
 package gr.aueb.cf.ch12;
 
+/**
+ * Very simple data class.
+ * Data, Default Constructor, Getters & Setters,
+ * are called JavaBean
+ */
 public class Student {
-    public int id;
-    public String firstname;
-    public String lastname;
+    private int id;
+    private String firstname;
+    private String lastname;
+
+    //DEFAULT CONSTRUCTOR
+    public Student(){
+
+    }
+
+    public Student(int id, String firstname, String lastname) {
+        this.id = id;
+        this.firstname = firstname;
+        this.lastname = lastname;
+    }
+
+    public int getId(){
+        return id;
+    }
+    public void setId(int id){
+        this.id = id;
+    }
+    public String getFirstname(){
+        return firstname;
+    }
+    public void setFirstname(String firstname){
+        this.firstname = firstname;
+    }
+    public String getLastname(){
+        return lastname;
+    }
+    public void setLastname(String lastname){
+        this.lastname = lastname;
+    }
 }
