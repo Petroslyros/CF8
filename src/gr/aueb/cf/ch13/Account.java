12c03ed0d9b3565c91ab5b79e4d265cfa1125c81
Petros <petrosluros@gmail.com>
2025-05-21 19:43:16 +0300

    Account class

---
+++ src/gr/aueb/cf/ch13/Account.java
@@ -0,0 +1,132 @@
+package gr.aueb.cf.ch13;
+
+public class Account {
+    private int id;
+    private String iban;
+    private String firstname;
+    private String lastname;
+    private String ssn;
+    private double balance;
+
+
+    public Account(){
+
+    }
+
+    public Account(int id, String iban, String firstname, String lastname, String ssn, double balance) {
+        this.id = id;
+        this.iban = iban;
+        this.firstname = firstname;
+        this.lastname = lastname;
+        this.ssn = ssn;
+        this.balance = balance;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getIban() {
+        return iban;
+    }
+
+    public void setIban(String iban) {
+        this.iban = iban;
+    }
+
+    public String getFirstname() {
+        return firstname;
+    }
+
+    public void setFirstname(String firstname) {
+        this.firstname = firstname;
+    }
+
+    public String getLastname() {
+        return lastname;
+    }
+
+    public void setLastname(String lastname) {
+        this.lastname = lastname;
+    }
+
+    public String getSsn() {
+        return ssn;
+    }
+
+    public void setSsn(String ssn) {
+        this.ssn = ssn;
+    }
+
+    public double getBalance() {
+        return balance;
+    }
+
+    public void setBalance(double balance) {
+        this.balance = balance;
+    }
+
+    //PUBLIC API
+
+    /**
+     * Deposits a certain amount of money
+     * @param amount the amount of money to be deposited
+     * @throws Exception if the amount is negative
+     */
+    public void deposit(double amount)throws Exception{
+        try {
+          if(amount < 0){
+              throw new Exception("Negative amount is not valid.");
+          }
+          balance += amount;
+        } catch (Exception e){
+            System.err.println("Negative amount : " + amount + "is not valid." + e);
+            throw e;
+        }
+    }
+
+    /**
+     * Withdraws a certain amount of money from {@link Account}
+     * First checks if SSN is valid by caling {@link Account#isSsnValid(String)}
+     * @param amount to be withdrawn
+     * @param ssn check if ssn is valid
+     * @throws Exception
+     */
+    public void withdraw(double amount, String ssn) throws Exception{
+        try{
+            if(amount < 0){
+                throw  new Exception("Cannot withdraw negative amount");
+            } else if(balance < amount){
+                throw new Exception("Insufficient funds");
+            } else if(!isSsnValid(ssn)){
+                throw new Exception("SSN is not valid");
+            }
+            balance -= amount;
+        } catch (Exception e){
+            System.err.println("Invalid action" + e);
+            throw e;
+        }
+    }
+    private boolean isSsnValid(String ssn){
+        return ssn.equals(this.ssn);
+    }
+
+    /**
+     * returns the balance of the {@link Account}
+      * @return
+     */
+    public double getAccountBalance(){
+        return getBalance();
+    }
+
+    /**
+     * @return  provides the user with the account information of id, iban, firstname, lastname, ssn, balance
+     */
+    public String accountToString(){
+        return "(" + getId() + ", " + iban + ", " + firstname + ", " + lastname + ", " + ssn + ", " + balance + ")";
+    }
+}
---
+++ src/gr/aueb/cf/ch13/Main.java
@@ -0,0 +1,20 @@
+package gr.aueb.cf.ch13;
+
+public class Main {
+    public static void main(String[] args) {
+
+        Account aliceAccount = new Account(1, "GR123456", "Alice", "W.","F0654321",100);
+
+        try{
+            aliceAccount.deposit(500);
+            System.out.println(aliceAccount.accountToString());
+            aliceAccount.withdraw(50,"F0654321");
+
+            System.out.println(aliceAccount.accountToString());
+            aliceAccount.withdraw(100, "AB123"); //throws exception
+
+        } catch (Exception e){
+            System.out.println(e.getMessage());
+        }
+    }
+}
