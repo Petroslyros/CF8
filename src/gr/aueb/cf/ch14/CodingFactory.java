43919cbe48ed5b4e2cd4417e37d428a9906dbaf3
Petros <petrosluros@gmail.com>
2025-05-21 20:32:10 +0300

    Account class

---
+++ src/gr/aueb/cf/ch14/CodingFactory.java
@@ -0,0 +1,12 @@
+package gr.aueb.cf.ch14;
+
+public class CodingFactory {
+    private static final CodingFactory INSTANCE= new CodingFactory(); //pre defined instance
+
+    private CodingFactory(){
+
+    }
+    public static CodingFactory getInstance(){
+        return INSTANCE;
+    }
+}
