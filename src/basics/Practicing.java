ef8cfcc999569ff107d7fba58bf8d4920a7fd7d8
Petros <petrosluros@gmail.com>
2025-05-22 11:13:39 +0300

    prac

--- src/basics/Practicing.java
+++ src/basics/Practicing.java
@@ -4,6 +4,7 @@ package basics;
 
 public class Practicing {
     public static void main(String[] args) {
+
         int[][] grid = {{2,3,4},{5,7,6},{4,3,1},{6,9,7},{4,6,3}};
         for(int i = 0; i < grid.length-1; i++){
             catchSpider(grid[i][0],grid[i][1],grid[i][2]);
