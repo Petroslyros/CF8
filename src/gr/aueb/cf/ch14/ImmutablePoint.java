b7b07867641886aed21984f5c3030bd8ea07e277
Petros <petrosluros@gmail.com>
2025-05-26 20:36:13 +0300

    Inheritance

---
+++ src/gr/aueb/cf/ch14/ImmutablePoint.java
@@ -0,0 +1,24 @@
+package gr.aueb.cf.ch14;
+
+public class ImmutablePoint {
+
+    private final int x;
+    private final int y;
+
+    public ImmutablePoint(){
+        x = 0;
+        y = 0;
+    }
+    public ImmutablePoint(int x, int y){
+        this.x = 0;
+        this.y = 0;
+    }
+
+    public int getX() {
+        return x;
+    }
+
+    public int getY() {
+        return y;
+    }
+}
