6bf464c24faabfb164b0104547a11ee931766e5f
Petros <petrosluros@gmail.com>
2025-06-04 20:59:11 +0300

    controller

---
+++ src/gr/aueb/cf/ch19/mobile_contacts/Main.java
@@ -0,0 +1,12 @@
+package gr.aueb.cf.ch19.mobile_contacts;
+
+import gr.aueb.cf.ch19.mobile_contacts.dao.IMobileContactDAO;
+import gr.aueb.cf.ch19.mobile_contacts.model.MobileContact;
+
+public class Main {
+    private final IMobileContactDAO dao = new MobileContactDaoImpl();
+
+    public static void main(String[] args) {
+        MobileContact mobileContact = new MobileContact()
+    }
+}
