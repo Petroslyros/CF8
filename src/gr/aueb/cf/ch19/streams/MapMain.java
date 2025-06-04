fba78d410c2a494320d07d8e7e43295826929201
Petros <petrosluros@gmail.com>
2025-06-04 19:00:52 +0300

    streams

---
+++ src/gr/aueb/cf/ch19/streams/MapMain.java
@@ -0,0 +1,10 @@
+package gr.aueb.cf.ch19.streams;
+
+import java.util.List;
+
+public class MapMain {
+
+    public static void main(String[] args) {
+        List<Product> products = List.of(new Product())
+    }
+}
