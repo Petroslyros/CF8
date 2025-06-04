58ca76f684d20a0ef207e9649fc641482ae7bbef
Petros <petrosluros@gmail.com>
2025-06-04 18:09:54 +0300

    comparator

---
+++ src/gr/aueb/cf/ch19/maps/MainMap.java
@@ -0,0 +1,32 @@
+package gr.aueb.cf.ch19.maps;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class MainMap {
+
+    public static void main(String[] args) {
+        Map<String, String> countries = new HashMap<>();
+
+        // add
+        countries.put("GR", "Greece");
+        countries.put("US", "United States");
+        countries.put("EN", "England");
+
+        //update
+        countries.put("US", "U. States");
+
+        //get
+        String value = countries.get("GR"); //GREECE
+
+        //remove
+        countries.remove("GR");
+
+        //iterate
+        for (Map.Entry<String, String> entry: countries.entrySet()){
+            System.out.println("Key : " + entry.getKey() + "Value" + entry.getValue());
+        }
+
+        countries.forEach((k,v) -> System.out.println("Key " + k + "Value: " + v));
+    }
+}
